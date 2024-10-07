4209e7a0411b7cdef7857a232a89435755db6452
SAFFFFFFF7 <saf.alg699@gmail.com>
2024-10-07 10:23:59 +0200

    add/edit/delete medical history

--- Controllers/MedicalHistoryController.cs
+++ Controllers/MedicalHistoryController.cs
@@ -1,14 +1,105 @@
+using MEDManager.Data;
+using MEDManager.Models;
 using Microsoft.AspNetCore.Mvc;
-
 namespace MEDManager.Controllers
 {
-    public class MedicalHistory : Controller
+    public class MedicalHistoryController : Controller
     {
-        // GET: MedicalHistory
+        private readonly ApplicationDbContext _dbContext;
+
+        public MedicalHistoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: AllergyController
         public ActionResult Index()
+        {
+            return View(_dbContext.MedicalHistories);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Add(MedicalHistory medicalHistory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _dbContext.MedicalHistories.Add(medicalHistory);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            MedicalHistory? mediHist = _dbContext.MedicalHistories.FirstOrDefault<MedicalHistory>(mh => mh.Id == id);
+
+            if (mediHist != null)
+            {
+                return View(mediHist);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+
+        public IActionResult DeleteConfirmed(int Id)
+        {
+            MedicalHistory? mediHist = _dbContext.MedicalHistories.FirstOrDefault<MedicalHistory>(mh => mh.Id == Id);
+
+            if (mediHist != null)
+            {
+                _dbContext.MedicalHistories.Remove(mediHist);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            MedicalHistory? mediHist = _dbContext.MedicalHistories.FirstOrDefault<MedicalHistory>(mh => mh.Id == id);
+
+            if (mediHist != null)
+            {
+                return View(mediHist);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MedicalHistory medicalHistory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            MedicalHistory? mediHist = _dbContext.MedicalHistories.FirstOrDefault<MedicalHistory>(mh => mh.Id == medicalHistory.Id);
+
+            if (mediHist != null)
+            {
+                mediHist.Name = medicalHistory.Name;
+
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+
     }
 }
+
--- obj/Debug/net8.0/MEDManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MEDManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20aac08c404a68cf8300d55a6f222a4bdb1b2349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0689237b4694a2c0516bb882db64b00efe0582c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
