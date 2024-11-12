a4c6cae4e892f0aa161998e6e1a04579e5b7c30c
SAFFFFFFF7 <saf.alg699@gmail.com>
2024-11-12 22:12:46 +0100

    wip before the pdf

--- obj/Debug/net8.0/MEDManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/MEDManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MEDManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c94da561382546c2c2e1aea485ac5cedefc64ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1dbe073a39eaa755180d595e7e0309b16a31be")]
 [assembly: System.Reflection.AssemblyProductAttribute("MEDManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MEDManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
